53eb7eb83806100932942ff3e47ac15b7ed88f59
perej222 <31454212+perej222@users.noreply.github.com>
2017-09-07 17:07:08 -0400

    FIRST AND SECOND HOMEWORK

---
+++ HW2/Assets/MoveFiveUnitsFromX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//declare a variable for speed
+
+public class MoveFiveUnitsFromX : MonoBehaviour {
+
+	//delare variable
+	public float MoveSpeedFloatBox;
+	public float RotateSpeedFloatBoxs;
+
+
+	//Initialization
+	void Start () {
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		float MOVE = Input.GetAxisRaw ("Horizontal") * MoveSpeedFloatBox ;
+		float ROTATE = Input.GetAxisRaw ("Vertical") * RotateSpeedFloatBoxs;
+		transform.Rotate (ROTATE, 0, 0);
+		transform.Translate (MOVE, 0, 0);
+
+	}
+}
